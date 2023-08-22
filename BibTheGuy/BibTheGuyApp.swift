e19da6e23a36b2ccf3b72cee67f75490b764d4f6
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-22 13:51:06 -0400

    Initial Commit

---
+++ BibTheGuy/BibTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibTheGuyApp.swift
+//  BibTheGuy
+//
+//  Created by Francesca MACDONALD on 2023-08-22.
+//
+
+import SwiftUI
+
+@main
+struct BibTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
